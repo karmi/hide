ac5b85f742be4e98d7a61578a708d9fcbf3be32f
Karel Minarik <karmi@karmi.cz>
2011-02-09 17:25:43 +0100

    Corrected Deagol::Page

--- lib/deagol/page.rb
+++ lib/deagol/page.rb
@@ -1,11 +1,31 @@
 module Deagol
   class Page
 
-    attr_reader :title, :path, :content, :meta
+    attr_reader :path, :content, :meta
 
     def initialize(title, path, content)
       @title, @path, @content = title, path, content
       parse_metadata
+    end
+
+    def title
+      meta['title'] || @title
+    end
+
+    def category
+      meta['cat'] || 'page'
+    end
+
+    def id
+      path.tr('/', '__').gsub(/\..+$/, '')
+    end
+
+    def to_hash
+      { :id => id, :title => title, :category => category, :content => content, :path => path  }
+    end
+
+    def to_indexed_json
+      to_hash.to_json
     end
 
     private
--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -11,7 +11,7 @@ module Deagol
       end
 
       should "should save title, path and content" do
-        assert_equal '2010 02 08 youknowforsearch', @page.title
+        assert_equal 'You Know, for Search', @page.title
         assert_equal 'blog/_posts/2010-02-08-youknowforsearch.textile', @page.path
         assert_match Regexp.new(Regexp.escape("You Know, for Search")), @page.content
       end
@@ -20,6 +20,15 @@ module Deagol
         assert_not_nil @page.meta
         assert_equal 'blog', @page.meta['cat']
         assert_equal 'You Know, for Search', @page.meta['title']
+      end
+
+      should "have a :to_indexed_json method" do
+        assert_nothing_raised do
+          json = @page.to_indexed_json
+          hash = JSON.parse(json)
+          assert_equal 'You Know, for Search', hash['title']
+          assert_equal 'blog', hash['category']
+        end
       end
 
     end
