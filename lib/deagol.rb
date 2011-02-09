12f237084c3380adde497bc2766442889db5017c
Karel Minarik <karmi@karmi.cz>
2011-02-09 17:25:55 +0100

    Added Deagol::Indexer. Usage below.
        $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
    
        require 'rubygems'
        require File.expand_path('../../lib/deagol', __FILE__)
    
        wiki = Deagol::Wiki.new('/Users/karmi/Playground/ElasticSearch/Website/deagol/test/wiki')
        indexer = Deagol::Indexer.new(wiki, 'deagol-test')
    
        indexer.index_all_pages!

--- lib/deagol.rb
+++ lib/deagol.rb
@@ -1,8 +1,10 @@
 require 'gollum'
+require 'slingshot'
 require 'yaml'
 
 require 'deagol/wiki'
 require 'deagol/page'
+require 'deagol/indexer'
 
 module Deagol
 end
---
+++ lib/deagol/indexer.rb
@@ -0,0 +1,23 @@
+module Deagol
+  class Indexer
+
+    def initialize(wiki, index_name)
+      @wiki       = wiki
+      @index_name = index_name
+    end
+
+    def index_all_pages!
+      index = Slingshot.index @index_name
+      index.delete
+      index.create
+
+      @wiki.pages.each do |page|
+        p [page.category, page.id]
+        p index.store( page.category, page)
+      end
+
+      index.refresh
+    end
+
+  end
+end
