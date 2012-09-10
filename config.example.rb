66a3f0b5844e158938e35959187d52ae45cb948d
Clinton Gormley <clint@traveljury.com>
2012-09-10 15:39:53 +0200

    Added a custom analyzer for "content" (ngrams, word delimiter)

--- config.example.rb
+++ config.example.rb
@@ -7,17 +7,33 @@ module Hide
     :directories => ['guide', 'community', 'tutorials', 'videos'],
     :index_name  => 'elastic-search-website',
 
-    :mappings    => {
+    :settings => {
+       :analysis => {
+          :filter => {
+             :ngrams => {:type => 'edgeNGram', :max_gram => 30},
+             :word_delim => {:type => 'word_delimiter', :preserve_original => 1},
+          },
+          :analyzer => {
+             :content => {
+                :filter => ['standard','lowercase','word_delim','ngrams'],
+                :type => 'custom',
+                :tokenizer => 'standard'
+             }
+          }
+       }
+    },
+
+    :mappings => {
       :guide => {
         :properties => {
-          :title    => { :type => 'string', :boost => 2.0,            :analyzer => 'snowball' },
-          :content  => { :type => 'string', :analyzer => 'snowball' },
-          :id       => { :type => 'string', :index => 'not_analyzed', :include_in_all => false },
-          :url      => { :type => 'string', :index => 'not_analyzed', :include_in_all => false },
-          :category => { :type => 'string', :analyzer => 'keyword',   :include_in_all => false },
-          
+          :title => { :type => 'string', :boost => 2.0, :analyzer => 'content' },
+          :content => { :type => 'string', :analyzer => 'content' },
+          :id => { :type => 'string', :index => 'not_analyzed', :include_in_all => false },
+          :url => { :type => 'string', :index => 'not_analyzed', :include_in_all => false },
+          :category => { :type => 'string', :analyzer => 'keyword', :include_in_all => false },
+
         },
-        :_all => { :analyzer => 'snowball' }
+        :_all => { :analyzer => 'content' }
       }
     },
 
